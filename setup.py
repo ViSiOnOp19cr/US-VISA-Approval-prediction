5eed6bd42c5ec47a2c4934937d8ec8736953a246
chandan cr <chandancr515@gmail.com>
2024-03-07 19:58:37 +0530

    update

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="chandan",
+    author_email="chandancr515@gmail.com",
+    packages=find_packages()
+)
